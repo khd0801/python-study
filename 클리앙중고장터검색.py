babf5cc27888c5696e5aeb1531f2ca90acf285fe
khd0801 <khd0801@naver.com>
2022-03-31 11:51:05 +0900

    목요일 크롤링 클리앙 파일 저장

--- 클리앙중고장터검색.py
+++ 클리앙중고장터검색.py
@@ -1,31 +1,42 @@
 # coding:utf-8
 from bs4 import BeautifulSoup
 import urllib.request
+#정규표현식으로 검색
 import re 
 
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
+#파일로 저장
+f = open("c:\\work\\clien.txt", "wt", encoding="utf-8")
+#페이징 처리를 0번에서 9번까지
 for n in range(0,10):
         #클리앙의 중고장터 주소 
         data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+        print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
                                     headers = hdr)
         data = urllib.request.urlopen(req).read()
+        #한글이 꺠지는 경우는 디코딩이 필요
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
-        list = soup.findAll('a', attrs={'class':'list_subject'})
+
+        # <span class="subject_fixed" data-role="list-title-text" title="[가격조정] wii 밸런스 보드 박풀">
+        # 							[가격조정] wii 밸런스 보드 박풀
+        # 						</span>
+        #find_all(태그, attributes(속성들))
+        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
 
         for item in list:
                 try:
-                        #<a class='list_subject'><span>text</span><span>text</span>
-                        span = item.contents[1]
-                        span2 = span.nextSibling.nextSibling
-                        title = span2.text 
+                        title = item.text
+                        #print(title.strip())
                         if (re.search('아이폰', title)):
                                 print(title.strip())
-                                print('https://www.clien.net'  + item['href'])
+                                f.write(title.strip() + "\n")
+                        #         print('https://www.clien.net'  + item['href'])
                 except:
                         pass
-        
+f.close()
+print("크롤링 종료")
